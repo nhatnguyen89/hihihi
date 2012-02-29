c03e27d6b277dfab4ec54a9b82e1f12598db2b49
nhat nguyen <nhat_nguyen893008@yahoo.com>
2012-02-29 13:17:12 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hihihi::Application.config.secret_token = 'cf9665c0c0bf593be7306dbe9f6b7d92992e7206ce4faacef37d7415da86ddb07a8ba97612c99bec94e505f3aed878ab547aa4cf5de2b03d7cdb9c78d297aec7'
